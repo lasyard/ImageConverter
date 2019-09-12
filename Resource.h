052aeae130afe44010c052bcb7aa243ea6247236
Lasy <lasyard@yeah.net>
2019-09-12 16:49:43 +0800

    init

---
+++ Resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ImageConverter.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_IMAGECTYPE                  129
+#define IDD_CONVERT                     130
+#define IDC_FMT                         1002
+#define IDC_FMT_LST                     1004
+#define IDC_LIST1                       1005
+#define ID_CONVERT                      32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
